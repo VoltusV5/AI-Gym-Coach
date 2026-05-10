b58a9384e58b10238d70769e79792f7f99ec99d2
Никита <nikitagarkusha2006@gmail.com>
2026-05-10 09:11:49 +0300

    add AIapi and create handler

--- ai/chat/chat-ml-service.py
+++ ai/chat/chat-ml-service.py
@@ -1,33 +1,70 @@
 """
-От Go нужен вот такой формат сообщения:
-
-{
-"messages":[
-    {"role": "user", "content": "Привет"},
-    {"role": "assistant", "content": "Привет!"}
-]
-}
-и так далее, мне нужны последнии 8 сообщений, 4 пары получается
-
+Локальный ML-сервис для backend: POST /ai/user/chat → JSON ответ {"role":"assistant", "content": "Что то на нейронском"}.
+Запуск из папки ai:  python -m uvicorn chat.chat-ml-service:app --host 127.0.0.1 --port 5051
 """
+
+
 from fastapi import Body, FastAPI
-from transformers import pipeline
+from pydantic import BaseModel
+from cerebras.cloud.sdk import Cerebras
+from dotenv import load_dotenv
+from typing import Any, Dict, Optional
+import os
+
+def prompt(chat: list[dict], user,plan) -> list[dict[str,str]]:
+    """
+    Функция создающая промпт для нейроки и задающая её поведение.
+    """
+    system =  [{
+        "role": "system",
+        "content": f"""
+        Ты AI фитнес-ассистент.
+
+        Правила:
+        - Не давай медицинских диагнозов
+        - Не советуй опасные упражнения при наличии ограничений
+        - Отвечай кратко и структурировано
+        - Если пользователь новичок, не предлагай сложные упражнения
+        - Если возраст > 60, избегай высокоинтенсивных нагрузок
+        - Будь вежлив
+
+        Вот параметры человека: {user}
+        Вот тренировочный план его: {plan}
+
+        """
+    }]
+
+    message = system + chat[:10] + [{"role": "assistant", "content": ""}]
+    return message
 
 
 
-pipe = pipeline("text-generation", model="microsoft/Phi-3-mini-4k-instruct")
 
-def prompt(chat):
-    prompt = "".join([i["role"]+ ": " + i["content"] + "\n" for i in chat["messages"]])
-    prompt = "Ты ассистент. Отвечай КОРОТКО и по делу.\n\n" + prompt 
-    print(prompt)
-    return prompt
-message = {"messages":[
-    {"role": "Пользователь", "content": "Привет"},
-]}
-print(pipe(message))
+class ChatRequest(BaseModel):
+    messages: list
+    user: dict
+    plan: Optional[dict] = None
+
+
+load_dotenv()
+client = Cerebras(
+      api_key=os.getenv("CEREBRAS_API_KEY")
+  )
+
+
 app = FastAPI(title="sportik-ml-chat", version="0.1.0")
 
-@app.post("/user/chat")
-async def chat(chat):
-    pass
+@app.post("/ai/user/chat")
+async def ai_chat(chat:ChatRequest) -> Dict[str,str]:
+    completion = client.chat.completions.create(
+      messages=prompt(chat.messages, chat.user, chat.plan),
+      model="llama3.1-8b",
+      max_completion_tokens=1024,
+      temperature=0.2,
+      top_p=1,
+      stream=False
+    )
+    
+    return {"role":"assistant", "content": completion.choices[0].message.content}
+
+
